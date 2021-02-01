da71f4a729cebd47f4f76169160315c5169cde53
Gjianhao <kevinbina@163.com>
2021-02-02 00:22:41 +0800

    912.归并排序

---
+++ AlgorithmForC/MergeSort.c
@@ -0,0 +1,46 @@
+//
+//  MergeSort.c
+//  AlgorithmForC
+//
+//  Created by gjh on 2021/2/1.
+//
+
+#include "MergeSort.h"
+
+// 912.排序数组     必须掌握  归并排序
+void swap(int arr[], int a, int b) {
+    int tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
+
+void sort(int* arr, int left, int right) {
+    if (left == right) return;
+    // 分成两半
+    int mid = left + ((right - left) >> 1);
+    // 左边排序
+    sort(arr, left, mid);
+    // 右边排序
+    sort(arr, mid + 1, right);
+    merge(arr, left, mid + 1, right);
+}
+void merge(int* arr, int leftPtr, int rightPtr, int rightBound) {
+    // 中间值
+    int mid = rightPtr - 1;
+    int* temp = (int*)malloc(sizeof(int) * (rightBound - leftPtr + 1));
+    
+    // 定义三个指针
+    int i = leftPtr, j = rightPtr, k = 0;
+    
+    while (i <= mid && j <= rightBound) {
+        temp[k++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+    }
+    // 处理 i 或者 j 提前结束
+    while (i <= mid) temp[k++] = arr[i++];   // i 还有值
+    while (j <= rightBound) temp[k++] = arr[j++];   // j 还有值
+    
+    // 将 temp 赋值给 arr
+    for (int m = 0; m < (rightBound - leftPtr + 1); m++) {
+        arr[leftPtr + m] = temp[m];
+    }
+}
---
+++ AlgorithmForC/MergeSort.h
@@ -0,0 +1,14 @@
+//
+//  MergeSort.h
+//  AlgorithmForC
+//
+//  Created by gjh on 2021/2/1.
+//
+
+#ifndef MergeSort_h
+#define MergeSort_h
+
+#include <stdio.h>
+#include <stdlib.h>
+void merge(int* arr, int leftPtr, int rightPtr, int rightBound);
+#endif /* MergeSort_h */
